f7cd891152a069f46c0c7ed99a529fadde2c9fa9
Nicolas Schteinschraber <nicoschtein@gmail.com>
2014-11-12 09:57:38 -0300

    Added debug info

--- cyclic.py
+++ cyclic.py
@@ -1,12 +1,18 @@
 # cyclicGroup = [0, 1, 2, 3, 4, 5]
 cyclicGroup = [0, 1, 2, 3, 4]
 cyclicGroup.sort()
-
+debug = 1
 for number in cyclicGroup:
 	auxCyclicGroup = []
+	if debug:
+		print "testing number: " + str(number)
 	for testGen in xrange(1, len(cyclicGroup) + 1):
+		if debug:
+			print "    testgen: " + str(testGen)
 		congruent = (number * testGen) % len(cyclicGroup)
 		auxCyclicGroup.append(congruent)
+		if debug:
+			print "    cic: " + str(auxCyclicGroup)
 	auxCyclicGroup.sort()
 	if cyclicGroup == auxCyclicGroup:
-		print "Number " + str(number) + " is generator!"
+		print "  Number " + str(number) + " is generator!"
