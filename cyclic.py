98c7a4a3227561b16a4fdc8355e36d6ab5d1c942
Nicolas Schteinschraber <nicoschtein@gmail.com>
2014-11-12 09:50:09 -0300

    First version

--- cyclic.py
+++ cyclic.py
@@ -1,1 +1,12 @@
-# 
+# cyclicGroup = [0, 1, 2, 3, 4, 5]
+cyclicGroup = [0, 1, 2, 3, 4]
+cyclicGroup.sort()
+
+for number in cyclicGroup:
+	auxCyclicGroup = []
+	for testGen in xrange(1, len(cyclicGroup) + 1):
+		congruent = (number * testGen) % len(cyclicGroup)
+		auxCyclicGroup.append(congruent)
+	auxCyclicGroup.sort()
+	if cyclicGroup == auxCyclicGroup:
+		print "Number " + str(number) + " is generator!"
