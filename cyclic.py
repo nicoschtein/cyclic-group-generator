7d5807ac5bd0c4c24ccf91299d248b5d4773a178
Emiliano <emiliano.mallo@gmail.com>
2014-11-14 11:29:11 -0300

    ultimoBranch

--- cyclic.py
+++ cyclic.py
@@ -1,19 +1,32 @@
-# cyclicGroup = [0, 1, 2, 3, 4, 5]
-cyclicGroup = [1, 2, 3, 4]
+import copy
+import sys	
+import collections
+
+#init
+cyclicGroup=copy.deepcopy(sys.argv);
+showLogs=0
+if(cyclicGroup[1] == "debug"):
+	showLogs=1
+	cyclicGroup.pop(1)
+cyclicGroup.remove(cyclicGroup[0])
 cyclicGroup.sort()
-debug = 1
+	
+def checkIfNumberIsGenerator(number):
+	if showLogs:
+		print "testing number: " + str(number)
+	accum = int(number)
+	for cycle in xrange(1, len(cyclicGroup) + 1):
+		if showLogs:
+			print "    ("+ str(cycle) +") accum: " + str(accum)
+		accum = (int(accum) * int(number)) % (len(cyclicGroup) + 1)
+		auxCyclicGroup.append(str(accum))
+		if showLogs:
+			print "    cic: " + str(auxCyclicGroup)
+	auxCyclicGroup.sort() 
+	return collections.Counter(cyclicGroup) == collections.Counter(auxCyclicGroup)
+
 for number in cyclicGroup:
 	auxCyclicGroup = []
-	if debug:
-		print "testing number: " + str(number)
-	accum = number
-	for cycle in xrange(1, len(cyclicGroup) + 1):
-		if debug:
-			print "    ("+ str(cycle) +") accum: " + str(accum)
-		accum = (accum * number) % (len(cyclicGroup) + 1)
-		auxCyclicGroup.append(accum)
-		if debug:
-			print "    cic: " + str(auxCyclicGroup)
-	auxCyclicGroup.sort()
-	if cyclicGroup == auxCyclicGroup:
+	if checkIfNumberIsGenerator(number):
 		print "  Number " + str(number) + " is generator!"
+	
