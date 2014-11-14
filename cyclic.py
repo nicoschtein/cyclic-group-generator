0a152f2b3a643c797e7a07f9e8e4384f7332638d
emallo5 <emiliano.mallo@gmail.com>
2014-11-14 11:54:29 -0300

    Se agrego help

--- cyclic.py
+++ cyclic.py
@@ -2,10 +2,18 @@ import copy
 import sys	
 import collections
 
+if len(sys.argv) == 1:
+	print "\n#################################"
+	print "# Cyclic Group Generator Finder #"
+	print "#################################\n"
+	print "Usage  : " + sys.argv[0] + " --debug (optional) [group list] (required)"
+	print "Example: " + sys.argv[0] + " --debug 0 1 2 3 4\n"
+	sys.exit(0)
+
 #init
 cyclicGroup=copy.deepcopy(sys.argv);
 showLogs=0
-if(cyclicGroup[1] == "debug"):
+if(cyclicGroup[1] == "--debug"):
 	showLogs=1
 	cyclicGroup.pop(1)
 cyclicGroup.remove(cyclicGroup[0])
